e70e9226d7491d21cf3593fe01a6656025367537
Muhammed Izzet Saglam <m.izzetsaglam@gmail.com>
2025-03-22 00:48:42 +0300

    update

--- config/zsh/alias.zsh
+++ config/zsh/alias.zsh
@@ -1,6 +1,13 @@
-#!/bin/zsh
-
 # Functions
+function y() {
+  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
+
 custompip() {
   if ! command -v pip &>/dev/null; then
     echo "Error: pip is not installed or not in PATH." >&2
@@ -38,20 +45,21 @@ pipupdatewithpipreview() {
 }
 
 # alias
-alias v=nvim
+alias c='clear'
+alias v='nvim'
 alias sv='sudo nvim'
 alias install='sudo pacman -S --needed'
 alias remove='sudo pacman -R'
 alias update='yay --noconfirm'
-alias full-update='yay --devel'
+alias full-update='yay --devel && sudo pkgfile --update && sudo locate db >/dev/null'
 alias ssh_clear='rm ~/.ssh/known_hosts'
 alias ls='eza -a --icons --group-directories-first'
 alias ll='eza -al -h --color=always --icons --group-directories-first --octal-permissions'
 alias lt='eza -a --tree --level=1 --icons --group-directories-first'
 alias gs='git status'
 alias py='python'
-alias pipforce=custompip
-alias pipupdate=pipupdatewithpipreview
+alias pipforce='custompip'
+alias pipupdate='pipupdatewithpipreview'
 alias share='net usershare add'
 alias unshare='net usershare remove'
 alias sharelist='net usershare info'
@@ -74,3 +82,4 @@ alias win11shutdown='virsh --connect=qemu:///system shutdown win11'
 alias ff='fastfetch --config examples/12'
 alias integratedgpu='supergfxctl -m Integrated'
 alias hybridgpu='supergfxctl -m Hybrid'
+alias neo='LANG=C neo -D'
