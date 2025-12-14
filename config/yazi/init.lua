5c80fbd732966e24d5b4983fa0a20afcab59f1c7
Muhammed Izzet Saglam <m.izzetsaglam@gmail.com>
2025-12-14 23:05:14 +0300

    yazi güncelleştirildi.

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -1,4 +1,17 @@
+-- plugins:full-border
 require("full-border"):setup {
 	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
 	type = ui.Border.ROUNDED,
 }
+
+-- plugins:git
+th.git = th.git or {}
+th.git.modified_sign = "M"
+th.git.deleted_sign = "D"
+require("git"):setup()
+
+-- uhs-robert/recycle-bin
+require("recycle-bin"):setup({
+  -- Optional: Override automatic trash directory discovery
+  -- trash_dir = "~/.local/share/Trash/",  -- Uncomment to use specific directory
+})
